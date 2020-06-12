820c83fcd0cf5900f96f5aeb1ef04d574e79f226
github username <github email address>
2020-06-12 13:39:12 -0700

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+    validates :email,  presence: true,   uniqueness: { message: "must be unique please" }
+    validates :name, presence: true
+
+
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+    validates :title,  presence: true,   uniqueness: { message: "must have a title please" }
+    validates :content, length: { minimum: 100,
+    too_short: "%{count} characters is the minimum allowed" }
+    # validates :category,  presence: { message: "must have a valid category please" }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid catrgory" }
+
+
 end
+
